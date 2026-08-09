1e94600d8625fbcc6ae5cfbdc1bba5ebe05676dc
gilsar <konick.qlippoth@gmail.com>
2026-08-09 14:24:41 +0300

    197. Rising Temperature

---
+++ easy/_0197_Rising_Temperature/Solution.sql
@@ -0,0 +1,22 @@
+/* sql\leetcode-sql\tables\create_visits_transactions1.sql
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT weather.id
+  FROM weather
+  JOIN (SELECT id,
+               recordDate + INTERVAL 1 DAY AS recordDate,
+               temperature AS yesterday_temperature
+          FROM weather) AS yesterday_weather
+    USING (recordDate)
+ WHERE temperature > yesterday_temperature;
+
+SELECT w1.id
+  FROM weather AS w1
+       LEFT JOIN weather AS w2
+       ON w1.recordDate = w2.recordDate + INTERVAL 1 DAY
+ WHERE w1.temperature > w2.temperature;
