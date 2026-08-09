fac7660fa539953a4416038c6faaa7e7ffa4312b
gilsar <konick.qlippoth@gmail.com>
2026-08-09 21:55:01 +0300

    1789. Primary Department for Each Employee

---
+++ easy/1789. Primary Department for Each Employee/Solution.sql
@@ -0,0 +1,35 @@
+/*
+1789. Primary Department for Each Employee
+
+Employees can belong to multiple departments. When the employee joins other departments,
+they need to decide which department is their primary department.
+Note that when an employee belongs to only one department, their primary column is 'N'.
+
+Write a solution to report all the employees with their primary department. For employees who belong to one department, report their only department.
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT employee_id, department_id
+  FROM employee
+ WHERE primary_flag = 'Y'
+
+ UNION
+
+SELECT employee_id, department_id
+  FROM employee
+ WHERE employee_id IN
+       (SELECT employee_id
+          FROM employee
+         GROUP BY employee_id
+        HAVING COUNT(*) = 1);
+
+SELECT employee_id, department_id
+  FROM employee
+ WHERE primary_flag = 'Y'
+    OR employee_id IN
+       (SELECT employee_id
+          FROM employee
+         GROUP BY employee_id
+        HAVING COUNT(*) = 1);
