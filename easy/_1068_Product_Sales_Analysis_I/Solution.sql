69cf76319ba10e19c2af53348621c1f4cbfd2fbb
gilsar <konick.qlippoth@gmail.com>
2026-08-09 13:52:39 +0300

    1068. Product Sales Analysis I

---
+++ easy/_1068_Product_Sales_Analysis_I/Solution.sql
@@ -0,0 +1,13 @@
+/* sql\leetcode-sql\tables\create_employees.sql
+Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+Return the resulting table in any order.
+
+The result format is in the following example.
+*/
+
+
+SELECT product_name, year, price
+  FROM product
+  JOIN sales
+    ON product.product_id = sales.product_id;
