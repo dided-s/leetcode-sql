505ace815c8257cdf4e5186fccd874e1f73eaeae
gilsar <konick.qlippoth@gmail.com>
2026-08-09 18:13:18 +0300

    1211. Queries Quality and Percentage

---
+++ easy/1211. Queries Quality and Percentage/Solution.sql
@@ -0,0 +1,21 @@
+/*
+1211. Queries Quality and Percentage
+
+We define query quality as: The average of the ratio between query rating and its position.
+
+We also define poor query percentage as: The percentage of all queries with rating less than 3.
+
+Write a solution to find each query_name, the quality and poor_query_percentage.
+
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT query_name,
+       ROUND(AVG(rating / position), 2) AS quality,
+       ROUND(100 * AVG(if(rating < 3, 1, 0)), 2) AS poor_query_percentage
+  FROM queries
+ GROUP BY query_name;
