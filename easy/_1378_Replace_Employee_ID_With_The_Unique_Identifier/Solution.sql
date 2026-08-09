20f49e48f550ddcaeffe2af43860aa1b028f5d2d
gilsar <konick.qlippoth@gmail.com>
2026-08-09 13:46:39 +0300

    1378. Replace Employee ID With The Unique Identifier

---
+++ easy/_1378_Replace_Employee_ID_With_The_Unique_Identifier/Solution.sql
@@ -0,0 +1,12 @@
+/* sql\leetcode-sql\tables\create_employees.sql
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT unique_id, name
+  FROM employees
+       LEFT JOIN employeeuni
+       ON employees.id = employeeuni.id;
