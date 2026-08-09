aaca6d444d23f653bc20668110c2d99b59525955
gilsar <konick.qlippoth@gmail.com>
2026-08-09 16:16:06 +0300

    620. Not Boring Movies

---
+++ easy/620. Not Boring Movies/Solution.sql
@@ -0,0 +1,12 @@
+/*
+620. Not Boring Movies
+Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+Return the result table ordered by rating in descending order.
+The result format is in the following example.
+*/
+
+SELECT *
+  FROM cinema
+ WHERE id % 2 = 1
+   AND description NOT LIKE 'boring'
+ ORDER BY rating DESC;
