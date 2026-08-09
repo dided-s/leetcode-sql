95308554550bac8c378e9d25c6ec702bfbfb0800
gilsar <konick.qlippoth@gmail.com>
2026-08-09 20:33:14 +0300

    596. Classes With at Least 5 Students

---
+++ easy/596. Classes With at Least 5 Students/Solution.sql
@@ -0,0 +1,13 @@
+/*
+596. Classes With at Least 5 Students
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT class
+  FROM courses
+ GROUP BY class
+HAVING COUNT(*) >= 5;
