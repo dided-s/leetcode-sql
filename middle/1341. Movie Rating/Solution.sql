6363c67220aee7db521cf23d4fb8586f82baa39d
gilsar <konick.qlippoth@gmail.com>
2026-08-11 14:13:13 +0300

    1341. Movie Rating

---
+++ middle/1341. Movie Rating/Solution.sql
@@ -0,0 +1,28 @@
+/*
+1341. Movie Rating
+Write a solution to:
+
+Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+The result format is in the following example.
+*/
+
+(SELECT name AS result
+   FROM users
+        LEFT JOIN movierating
+        USING (user_id)
+  GROUP BY user_id, name
+  ORDER BY COUNT(movie_id) DESC, name ASC
+  LIMIT 1)
+
+  UNION
+
+(SELECT title AS result
+   FROM movies
+        LEFT JOIN movierating
+        USING (movie_id)
+  WHERE MONTH(created_at) = 02
+    AND YEAR(created_at) = 2020
+  GROUP BY movie_id, title
+  ORDER BY AVG(rating) DESC, title ASC
+  LIMIT 1);
