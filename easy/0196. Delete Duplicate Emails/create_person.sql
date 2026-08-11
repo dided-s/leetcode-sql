b520da57312f1d17a275215342bb367f52b32526
gilsar <konick.qlippoth@gmail.com>
2026-08-11 11:19:33 +0300

    196. Delete Duplicate Emails

---
+++ easy/0196. Delete Duplicate Emails/create_person.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Person (
+    Id    INT,
+    Email VARCHAR(255)
+);
+
+TRUNCATE TABLE Person;
+
+INSERT INTO Person (id, email)
+VALUES ('1', 'john@example.com');
+
+INSERT INTO Person (id, email)
+VALUES ('2', 'bob@example.com');
+
+INSERT INTO Person (id, email)
+VALUES ('3', 'john@example.com');
