9b795d159cf856a1737debd6f31d2836d6ab0e30
gilsar <konick.qlippoth@gmail.com>
2026-08-22 13:22:24 +0300

    586. Customer Placing the Largest Number of Orders

---
+++ easy/0586. Customer Placing the Largest Number of Orders/Solution.sql
@@ -0,0 +1,16 @@
+/*
+586. Customer Placing the Largest Number of Orders
+Write a solution to find the customer_number for the customer who has
+placed the largest number of orders.
+
+The test cases are generated so that exactly one customer will have
+placed more orders than any other customer.
+
+The result format is in the following example.
+*/
+
+SELECT customer_number
+  FROM orders
+ GROUP BY customer_number
+ ORDER BY COUNT(*) DESC
+ LIMIT 1;
