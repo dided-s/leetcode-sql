d2cc77af7c0b780bc0bfec39be4e56f575b5ffc6
gilsar <konick.qlippoth@gmail.com>
2026-08-22 11:40:57 +0300

    511. Game Play Analysis I

---
+++ easy/0511. Game Play Analysis I/Solution.sql
@@ -0,0 +1,13 @@
+/*
+511. Game Play Analysis I
+
+Write a solution to find the first login date for each player.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT player_id, MIN(event_date) AS first_login
+  FROM activity
+ GROUP BY player_id;
