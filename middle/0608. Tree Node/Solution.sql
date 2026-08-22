022537ce54086db302daf13dfafa8f1939e4264f
gilsar <konick.qlippoth@gmail.com>
2026-08-22 13:57:11 +0300

    608. Tree Node

---
+++ middle/0608. Tree Node/Solution.sql
@@ -0,0 +1,22 @@
+/*
+608. Tree Node
+
+Each node in the tree can be one of three types:
+
+"Leaf": if the node is a leaf node.
+"Root": if the node is the root of the tree.
+"Inner": If the node is neither a leaf node nor a root node.
+Write a solution to report the type of each node in the tree.
+
+Return the result table in any order.
+
+The result format is in the following example.
+*/
+
+SELECT id,
+       CASE
+       WHEN p_id IS NULL THEN 'Root'
+       WHEN id IN (SELECT p_id FROM tree) THEN 'Inner'
+       ELSE 'Leaf'
+       END as type
+  FROM tree;
